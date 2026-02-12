7708826baf96aab5c48700a8f7fb038015a01315
Viktor <25reyals@gmail.com>
2026-02-12 16:26:57 +0200

    Create global state Store

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+import Store from "./services/Store.js"
+
+// now how do we get the store to become global?
+// well one way is to
+// do this
+window.app = Store 
+// now the instance of Store will be available throughout the app through the windows global object
+
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded")
+    console.log("DOMContentLoaded", window.app)
 })
