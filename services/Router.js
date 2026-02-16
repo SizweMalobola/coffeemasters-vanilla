428e822f2c357b7aad1b53b9ea9811f6f464ac5c
Viktor <25reyals@gmail.com>
2026-02-16 18:26:45 +0200

    Make Router functional

--- services/Router.js
+++ services/Router.js
@@ -1,28 +1,53 @@
- const Router = {
-    init: function(){
-        // grab links
-        const linkElements = document.querySelectorAll("a.navlink");
-        // loop through all the a elements
-        linkElements.forEach(a => {
-            // add event listener 
-            a.addEventListener("click", (event) => {
-                event.preventDefault()
-                // grab href or url
-                const url = event.target.getAttribute("href")
-                // nav to href
-                this.goTo(url)
-            })
-        })
-        // initialize current path / route 
-        this.goTo(location.pathname)
-       
-    },
-    // add a function to nav to path/href in my app
-    goTo: function(path,addToHistory = true){
-        // addToHistory is replace
-        // addToHistory true == replace false
-        console.log(` go to ${path}`)
-    }
-}
+const Router = {
+	init: function () {
+		// grab links
+		const linkElements = document.querySelectorAll("a.navlink");
+		// loop through all the a elements
+		linkElements.forEach((a) => {
+			// add event listener
+			a.addEventListener("click", (event) => {
+				event.preventDefault();
+				// grab href or url
+				const url = event.target.getAttribute("href");
+				// nav to href
+				this.goTo(url);
+			});
+		});
+		// initialize current path / route
+		this.goTo("/");
+		this.goTo(location.pathname);
+		// add popState event handler for when thingy changes
+		window.addEventListener("popstate", (event) => {
+			// when url is changed
+		});
+	},
+	// add a function to nav to path/href in my app
+	goTo: (path, addToHistory = true) => {
+		// addToHistory is replace
+		// addToHistory true == replace false
+		if (addToHistory) {
+			history.pushState({ path }, "", path);
+		}
+		const mainEl = document.querySelector("main");
+		const newEl = document.createElement("h1");
+		switch (path) {
+			case "/":
+				newEl.textContent = "index";
+				break;
+			case "/order":
+				newEl.textContent = "order now";
+				break;
+			default:
+				if (path.startsWith("/product-")) {
+					newEl.textContent = "Details";
+					const paramId = path.substring(path.lastIndexOf("-") + 1);
+					newEl.dataset.id = paramId;
+				}
+				break;
+		}
+		mainEl.innerHTML = "";
+		mainEl.appendChild(newEl);
+	},
+};
 
-export default Router
+export default Router;
